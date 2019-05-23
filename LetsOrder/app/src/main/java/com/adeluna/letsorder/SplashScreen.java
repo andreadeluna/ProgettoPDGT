b514d69f6822f4cd693d28c34b815376595fe532
Andrea De Luna <andrea.deluna10@gmail.com>
2019-05-23 22:38:17 +0200

    Implementazione Activity SplashScreen

--- LetsOrder/app/src/main/java/com/adeluna/letsorder/SplashScreen.java
+++ LetsOrder/app/src/main/java/com/adeluna/letsorder/SplashScreen.java
@@ -3,7 +3,7 @@ package com.adeluna.letsorder;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class Splashscreen extends AppCompatActivity {
+public class SplashScreen extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
