55a802af30f61e00fa59e55105f0090f7a2c4871
Andrea De Luna <andrea.deluna10@gmail.com>
2019-05-27 00:05:32 +0200

    Implementazione metodo di Logout

--- LetsOrder/app/src/main/java/com/adeluna/letsorder/MainActivity.java
+++ LetsOrder/app/src/main/java/com/adeluna/letsorder/MainActivity.java
@@ -1,13 +1,86 @@
 package com.adeluna.letsorder;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+
+    private FirebaseAuth mAuth;
+
+    private DatabaseReference mDatabaseReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        mAuth = FirebaseAuth.getInstance();
+
+
+        // Write a message to the database
+        mDatabaseReference = FirebaseDatabase.getInstance().getReference();
+
     }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.layout_menu, menu);
+
+        return true;
+
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        int id = item.getItemId();
+
+        if(id == R.id.logoutItem){
+
+            Log.i(TAG, "Logout selezionato");
+
+            // Logout
+
+            mAuth.signOut();
+
+            updateUI();
+
+            return true;
+
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    private void updateUI() {
+
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+
+        if(currentUser == null){
+
+            Intent intToLogin = new Intent(this, LoginActivity.class);
+            finish();
+
+            startActivity(intToLogin);
+
+        }
+
+    }
+
 }
