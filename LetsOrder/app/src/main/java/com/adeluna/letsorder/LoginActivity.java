22373070b7b4e062e7ae8d4629815c63ae8357e2
Massimiliano Montaleone <theplayergame97@gmail.com>
2019-06-22 01:18:26 +0200

    resource update, update RegisterActivity, cleanup code

--- LetsOrder/app/src/main/java/com/adeluna/letsorder/LoginActivity.java
+++ LetsOrder/app/src/main/java/com/adeluna/letsorder/LoginActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -15,6 +16,8 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 import java.util.Objects;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -24,8 +27,9 @@ public class LoginActivity extends AppCompatActivity {
 
     // Variabili
 
-    EditText mNomeUtente;
-    EditText mPassword;
+    EditText mNomeUtente, mPassword;
+    TextView tvLogin;
+    Button btnRegistrati;
     private FirebaseAuth mAuth;
 
     @Override
@@ -38,7 +42,53 @@ public class LoginActivity extends AppCompatActivity {
         mTitle.setText(R.string.title_activity_login);
         Objects.requireNonNull(getSupportActionBar()).setDisplayShowTitleEnabled(false);
 
+        initUI();
+
         mAuth = FirebaseAuth.getInstance();
+
+        tvLogin.setOnClickListener(v -> {
+            Log.d("LoginActivity1", "Registrati Click");
+            finish();
+            startActivity(new Intent(LoginActivity.this, RegisterActivity.class));
+        });
+
+
+        btnRegistrati.setOnClickListener(v -> {
+
+            Log.d("LoginActivity1", "Login Button Click");
+
+            String nomeUtente = mNomeUtente.getText().toString();
+            String password = mPassword.getText().toString();
+
+            Log.d("LoginActivity1", nomeUtente);
+            Log.d("LoginActivity1", password);
+
+            if (!(nomeUtente.length() > 7) || !emailValida(nomeUtente)) {
+
+                Snackbar snack = Snackbar.make(v, "E-mail non valida", Snackbar.LENGTH_LONG);
+                SnackbarHelper.configSnackbar(v.getContext(), snack);
+                snack.show();
+
+            } else if (!(password.length() > 7)) {
+
+                Snackbar snack = Snackbar.make(v, "Password non valida", Snackbar.LENGTH_LONG);
+                SnackbarHelper.configSnackbar(v.getContext(), snack);
+                snack.show();
+
+            } else
+                loginUser(nomeUtente, password, v);
+
+
+        });
+    }
+
+    private void initUI() {
+
+        tvLogin = findViewById(R.id.tvLogin);
+        btnRegistrati = findViewById(R.id.btnRegistrati);
+        mNomeUtente = findViewById(R.id.etRegName);
+        mPassword = findViewById(R.id.etRegPass);
+
     }
 
     @Override
@@ -71,42 +121,13 @@ public class LoginActivity extends AppCompatActivity {
     }
 
 
-    public void btnLoginClick(View view) {
-
-        Log.d("LoginActivity1", "Login Button Click");
-
-        // Collegare le variabili ai Widgets
-
-        mNomeUtente = findViewById(R.id.etRegName);
-        mPassword = findViewById(R.id.etRegPass);
-
-        String nomeUtente = mNomeUtente.getText().toString();
-        String password = mPassword.getText().toString();
-
-        Log.d("LoginActivity1", nomeUtente);
-        Log.d("LoginActivity1", password);
-
-        if (!(nomeUtente.length() > 7) || !(nomeUtente.contains("@"))) {
-
-            Snackbar snack = Snackbar.make(view, "E-mail non valida", Snackbar.LENGTH_LONG);
-            SnackbarHelper.configSnackbar(view.getContext(), snack);
-            snack.show();
-
-        } else if (!(password.length() > 7)) {
-
-
-            Snackbar snack = Snackbar.make(view, "Password non valida", Snackbar.LENGTH_LONG);
-            SnackbarHelper.configSnackbar(view.getContext(), snack);
-            snack.show();
-
-        } else {
-
-            loginUser(nomeUtente, password, view);
-
-        }
-
+    private boolean emailValida(String email) {
+        String expression = "^[\\w\\.]+@([\\w]+\\.)+[A-Z]{2,7}$";
+        CharSequence inputString = email;
+        Pattern pattern = Pattern.compile(expression, Pattern.CASE_INSENSITIVE);
+        Matcher matcher = pattern.matcher(inputString);
+        return matcher.matches();
     }
-
 
     private void loginUser(String email, String password, View view) {
 
@@ -132,13 +153,5 @@ public class LoginActivity extends AppCompatActivity {
 
     }
 
-
-    public void tvRegistratiClick(View view) {
-
-        Log.d("LoginActivity1", "Registrati Click");
-        finish();
-        startActivity(new Intent(this, RegisterActivity.class));
-
-    }
 
 }
