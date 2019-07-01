4d5913f6b0ed58d6b4fb0299c6b95f53a10d2f7c
Andrea De Luna <andrea.deluna10@gmail.com>
2019-07-01 23:34:01 +0200

    Aggiunta file settings

---
+++ LetsOrderBot/settings_dist.py
@@ -0,0 +1,18 @@
+﻿# Your Bot token (get it from BotFather)
+token = 'TOKEN'
+
+# Message send when user 'start' the bot
+start_msg = "Ciao e benvenuto su @LetsOrderBot! \n"\
+            "Sono un semplice bot che ti permetterà di "\
+            "cercare e di reperire informazioni "\
+            "sui ristoranti nelle vicinanze. "\
+            "Non dovrai far altro che inserire la posizione "\
+            "e il ristorante da te cercato! Se vorrai, "\
+            "potrai anche visualizzare le recensioni dei clienti! "\
+            "Divertiti! 🥘"\
+
+# File with 'started' clients
+client_file = "clients_id.txt"
+
+# Url for api
+url_api = 'url'
